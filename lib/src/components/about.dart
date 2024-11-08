148de94399fdcafee5e57665a04e32c6f33bc27b
Kelvin <thisisrandom838@gmail.com>
2024-11-09 01:55:58 +0800

    wrap large text in about in fittedbox

--- lib/src/components/about.dart
+++ lib/src/components/about.dart
@@ -28,94 +28,119 @@ class About extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 16),
-          Container(
-            padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 0.0),
-            child: RichText(
-              text: TextSpan(
-                text: "Hello, I am ",
-                style: GoogleFonts.montserrat(
-                  textStyle: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: AdaptiveTheme.of(context).theme.colorScheme.onSurface,
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 0.0),
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Hello, I am ",
+                      style: GoogleFonts.montserrat(
+                        textStyle: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: AdaptiveTheme.of(context)
+                              .theme
+                              .colorScheme
+                              .onSurface,
+                        ),
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "Kelvin, CHAN",
+                          style: GoogleFonts.dmSerifText(
+                            textStyle: TextStyle(
+                              fontSize: 32,
+                              color: AdaptiveTheme.of(context)
+                                  .theme
+                                  .colorScheme
+                                  .secondary,
+                            ),
+                          ),
+                        ),
+                        TextSpan(
+                          text: ".",
+                          style: GoogleFonts.montserrat(
+                            textStyle: TextStyle(
+                              fontSize: 28,
+                              color: AdaptiveTheme.of(context)
+                                  .theme
+                                  .colorScheme
+                                  .onSurface,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: "Kelvin, CHAN",
-                    style: GoogleFonts.dmSerifText(
-                      textStyle: TextStyle(
-                        fontSize: 32,
-                        color:
-                            AdaptiveTheme.of(context).theme.colorScheme.secondary,
+                const SizedBox(height: 12),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 0.0),
+                  child: RichText(
+                    text: TextSpan(
+                      text: "I'm also known as ",
+                      style: GoogleFonts.montserrat(
+                        textStyle: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: AdaptiveTheme.of(context)
+                              .theme
+                              .colorScheme
+                              .onSurface,
+                        ),
                       ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "3_n",
+                          style: GoogleFonts.dmSerifText(
+                            textStyle: TextStyle(
+                              fontSize: 32,
+                              wordSpacing: 1.2,
+                              color: AdaptiveTheme.of(context)
+                                  .theme
+                                  .colorScheme
+                                  .secondary,
+                            ),
+                          ),
+                        ),
+                        TextSpan(
+                          text: " online.",
+                          style: GoogleFonts.montserrat(
+                            textStyle: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: AdaptiveTheme.of(context)
+                                  .theme
+                                  .colorScheme
+                                  .onSurface,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                  TextSpan(
-                    text: ".",
-                    style: GoogleFonts.montserrat(
-                      textStyle: TextStyle(
-                        fontSize: 28,
-                        color:
-                            AdaptiveTheme.of(context).theme.colorScheme.onSurface,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(height: 12),
-          Container(
-            padding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 0.0),
-            child: RichText(
-              text: TextSpan(
-                text: "I'm also known as ",
-                style: GoogleFonts.montserrat(
-                  textStyle: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: AdaptiveTheme.of(context).theme.colorScheme.onSurface,
                   ),
                 ),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: "3_n",
-                    style: GoogleFonts.dmSerifText(
-                      textStyle: TextStyle(
-                        fontSize: 32,
-                        wordSpacing: 1.2,
-                        color:
-                            AdaptiveTheme.of(context).theme.colorScheme.secondary,
-                      ),
-                    ),
-                  ),
-                  TextSpan(
-                    text: " online.",
-                    style: GoogleFonts.montserrat(
-                      textStyle: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                        color:
-                            AdaptiveTheme.of(context).theme.colorScheme.onSurface,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              ],
             ),
           ),
           const SizedBox(height: 36),
           Container(
             padding: const EdgeInsets.fromLTRB(24.0, 0, 24.0, 0),
             child: SizedBox(
-              width: MediaQuery.of(context).size.width - 48 * 2 > 0 ? MediaQuery.of(context).size.width - 48 * 2 : 0,
+              width: MediaQuery.of(context).size.width - 48 * 2 > 0
+                  ? MediaQuery.of(context).size.width - 48 * 2
+                  : 0,
               child: Text(
                 "I'm currently an undergraduate student studying computer science with an extended major in Artificial Intelligence. I couldn't say I'm good at programming, but I do enjoy the process of creating stuff and apps for people, hence I picked up this \"career\". I'm also interested in games (especially rhythm games!). Recently, I've been learning about depression disorders and how to live with friends who have self-harm behaviors.",
                 style: GoogleFonts.montserrat(
                   textStyle: TextStyle(
                     fontSize: 18,
-                    color: AdaptiveTheme.of(context).theme.colorScheme.onSurface,
+                    color:
+                        AdaptiveTheme.of(context).theme.colorScheme.onSurface,
                   ),
                 ),
               ),
