4cf28e77f82ba2de1c1224cee68446d2e3041e95
Kelvin <thisisrandom838@gmail.com>
2025-02-17 17:19:39 +0800

    Create footer_link.dart

---
+++ lib/src/text/footer_link.dart
@@ -0,0 +1,7 @@
+const Map<String, String> links = {
+  "email": "mailto:cychandt@connect.ust.hk",
+  "linkedin": "https://www.linkedin.com/in/chung-yuk-chan-6a2742241/",
+  "facebook": "https://www.facebook.com/chungyuk.chan.5",
+  "instagram": "https://www.instagram.com/f.prime.of.x/",
+  "github": "https://github.com/3underscoreN",
+};
